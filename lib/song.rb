413d0e81a8a6c015d8b78b6638f1d864ac54fe3f
chic-caller-7520 <arbrown110@gmail.com>
2020-04-14 04:01:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -65,6 +65,8 @@ class Song
     song
   end
   
-  def self.destroy_all
+  def self.destroy_all()
+    #reset the state of the @@all class variable to an empty array thereby deleting all previous song instances.
     @@all.clear
   end
+
